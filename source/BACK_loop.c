7c9c3a04f03a5d5660c8d4fd16686992224b00d3
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2017-05-25 09:26:18 +0200

    Popravil izklopno proceduro (upam)

--- source/BACK_loop.c
+++ source/BACK_loop.c
@@ -204,9 +204,15 @@ void work_fcn(void)
     if ((en_tipka == TRUE))
     {
         // postavim generator rampe v izhodišèe
-        // TODO v dolocenih primerih tole dela narobe
         DINT;
-        tok_bb_slew.Out = tok_bb1 + tok_bb2;
+        if ((tok_bb1 + tok_bb2) < 1.0)
+        {
+            tok_bb_slew = 0.0;
+        }
+        else
+        {
+            tok_bb_slew.Out = tok_bb1 + tok_bb2;
+        }
         state = Ramp_down;
         EINT;
     }
