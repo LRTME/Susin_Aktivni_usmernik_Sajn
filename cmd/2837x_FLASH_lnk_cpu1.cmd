bdc9fa758e7ad8706ef596ad344ea7f0f9553468
DenisS <denis.susin@fe.uni-lj.si>
2017-12-15 10:45:30 +0100

    - Dodan linker command file še za primer, ko program teče iz Flash   EEPROM-a, kar pomeni, da bo DCT regulator (oz. uporabljen FIR-DCT   filter) deloval tudi, ko bo povezava med računalnikom in uC   prekinjena.

--- cmd/2837x_FLASH_lnk_cpu1.cmd
+++ cmd/2837x_FLASH_lnk_cpu1.cmd
@@ -77,6 +77,9 @@ SECTIONS
 /* VARIABLES */
     .bss: >>        D_GS615  PAGE = 1
     .ebss: >>       D_GS615  PAGE = 1
+    firfilt > 		D_GS615  PAGE = 1			/* Section for struct for fast FIR filter realization with FPU unit */
+    firldb > 		D_GS615  PAGE = 1			/* Section for delay buffer for fast FIR filter realization with FPU unit - must be aligned, which is done with #pragma */
+   	coefffilt > 	D_GS615  PAGE = 1			/* Section for coeff buffer for fast FIR filter realization with FPU unit - must be aligned, which is done with #pragma */
 
 /* CODE */
     .text: >        P_FE,                           PAGE = 0, ALIGN(4)
